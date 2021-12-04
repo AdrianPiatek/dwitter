dfcec89ebdcfcf0e9abf08f08dfae8d87fa38fd4
Maciek <Your_Majesty@wp.pl>
2021-12-04 16:05:50 +0100

    extra folder for templates

--- main/views.py
+++ main/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-
 
 def home_page(response):
-    return render(response, 'Login.html')
+    return render(response, 'main/Login.html')
 
