d547e1e5e6c698e104f3e563b6bac80bba7a305c
ulpioro <78817508+ulpioro@users.noreply.github.com>
2021-12-06 21:06:55 +0100

    ADDED images to posts

--- dwitter/urls.py
+++ dwitter/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from main.forms import CustomAuthForm
 from django.contrib.auth import views as v
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,7 @@ urlpatterns = [
                                        authentication_form=CustomAuthForm), name='login'),
     path('logout/', v.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
